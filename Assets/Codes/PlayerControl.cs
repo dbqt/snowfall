dcedf2a10dd61bebf624c1a62e7c80e0d09917ed
David Tran <dt_dbqt@hotmail.com>
2014-12-31 23:36:39 -0500

    added control

---
+++ Assets/Codes/PlayerControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	public float moveSpeed = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		float direction = Input.GetAxis("Horizontal");
+		rigidbody.AddForce(Vector3.right * direction * moveSpeed);
+	}
+}
