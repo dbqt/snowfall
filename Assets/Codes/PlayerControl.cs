210c0eca2d8595e2560669f2d545e60aab7bb850
David Tran <dt_dbqt@hotmail.com>
2015-01-01 01:19:22 -0500

    control + gravity

--- Assets/Codes/PlayerControl.cs
+++ Assets/Codes/PlayerControl.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
-	public float moveSpeed = 1;
+	public float moveSpeed = 10;
 	
 	// Update is called once per frame
 	void Update () {
+
+		//keyboard input
 		float direction = Input.GetAxis("Horizontal");
+		//mouse or touch input will overwrite keyboard input
+		if (Input.GetMouseButton(0))
+		{
+			direction = Mathf.Sign( Input.mousePosition.x - (Screen.width/2) );
+		}
+		//apply force to move
 		rigidbody.AddForce(Vector3.right * direction * moveSpeed);
 	}
 }
---
+++ Assets/Codes/PlayerProperties.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProperties : MonoBehaviour {
+
+	public float rollSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.AddForce(Vector3.back * rollSpeed);
+	}
+}
