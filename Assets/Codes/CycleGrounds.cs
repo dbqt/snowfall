62ae6cdd52b370586a7a815d9727092f5c0cf7bd
David Tran <dt_dbqt@hotmail.com>
2015-01-01 01:57:31 -0500

    ground follows the player
    cycling ground panels

---
+++ Assets/Codes/CycleGrounds.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleGrounds : MonoBehaviour {
+
+	public Transform ground, player;
+
+	private float prevPos;
+	private Transform ground0, ground1;
+	private bool current; //fasle = 0, true = 1
+	private Quaternion q;
+
+	// Use this for initialization
+	void Start () {
+		prevPos = 0;
+		current = false;
+		q = ground.rotation;
+		ground0 = Instantiate(ground, Vector3.zero, q) as Transform;
+		ground1 = Instantiate(ground, new Vector3(100,100,100), q) as Transform; //very far!
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(prevPos - player.position.z > 3)
+			Recycle();
+	}
+
+	private void Recycle() {
+		if(current)
+		{	
+			Vector3 pos = ground1.position;
+			pos.z -= 10;
+			Object.Destroy(ground0.gameObject);
+			ground0 = Instantiate(ground, pos, q) as Transform;
+			prevPos = pos.z;
+		}
+		else
+		{
+			Vector3 pos = ground0.position;
+			pos.z -= 10;
+			Object.Destroy(ground1.gameObject);
+			ground1 = Instantiate(ground, pos, q) as Transform;
+			prevPos = pos.z;
+		}
+		current = !current;
+		
+	}
+}
--- Assets/Codes/PlayerProperties.cs
+++ Assets/Codes/PlayerProperties.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class PlayerProperties : MonoBehaviour {
 
 	public float rollSpeed = 1;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
+		//simulate gravity
 		rigidbody.AddForce(Vector3.back * rollSpeed);
+
+		//limit horizontal
+		Vector3 pos = this.gameObject.transform.position;
+		if(this.gameObject.transform.position.x < -9)
+			pos.x = -9;
+		if(this.gameObject.transform.position.x > 9)
+			pos.x = 9;
+		this.gameObject.transform.position = pos;
 	}
 }
