42b226a912b159589006a4a336ce0576c32e2866
David Tran <dt_dbqt@hotmail.com>
2015-01-01 22:11:49 -0500

    score system

--- Assets/Codes/GameLogic.cs
+++ Assets/Codes/GameLogic.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameLogic : MonoBehaviour {
 
-	public GameObject restartBtn, menuBtn;
-	private int score;
+	public GameObject restartBtn, menuBtn, player;
+	public Text t;
+	private float score;
 
 	// Use this for initialization
 	void Start () {
-	Time.timeScale = 1f;
-	restartBtn.SetActive(false);
-	menuBtn.SetActive(false);
+		Time.timeScale = 1f;
+		restartBtn.SetActive(false);
+		menuBtn.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		score = -player.transform.position.z;
+		t.text = ""+(int)score;
 	}
 
 	//called when player hit something
@@ -26,7 +29,7 @@ public class GameLogic : MonoBehaviour {
 	//gameover logic
 	private void GameOver(){
 		Debug.Log("Game Over!");
-		Time.timeScale = 0.1f;
+		Time.timeScale = 0f;
 		restartBtn.SetActive(true);
 		menuBtn.SetActive(true);		
 	}
