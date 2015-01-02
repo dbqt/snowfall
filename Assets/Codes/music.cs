d6ad1aeb203124acaf3eede22848426d2f8ca0e8
David Tran <dt_dbqt@hotmail.com>
2015-01-02 01:07:27 -0500

    music

---
+++ Assets/Codes/music.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class music : MonoBehaviour {
+//singleton
+	public static music instance = null;
+
+	// Use this for initialization
+	void Awake () {
+		if(instance != null && instance != this){
+			Destroy(this.gameObject);
+			return;
+		}
+		else{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+}
