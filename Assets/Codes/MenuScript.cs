b446878ee1a776b14fec63f364d20eddfca27527
David Tran <dt_dbqt@hotmail.com>
2015-01-01 21:37:52 -0500

    play and quit functional

---
+++ Assets/Codes/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	public string game, about;
+
+	public void PlayGame(){
+		Application.LoadLevel(game);
+	}
+
+	public void About(){
+		Application.LoadLevel(about);
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+
+}
